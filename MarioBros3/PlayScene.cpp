821f2c21a9354d5fd017d1938d30f7c594be5580
duongnhathuy99 <duongnhathuy2401@gmail.com>
2022-10-19 12:10:52 +0700

    create mushroom

--- MarioBros3/PlayScene.cpp
+++ MarioBros3/PlayScene.cpp
@@ -11,7 +11,7 @@
 #include "Platform.h"
 #include "Map.h"
 #include "QuestionBrick.h"
-
+#include "Mushroom.h"
 #include "SampleKeyEventHandler.h"
 
 using namespace std;
@@ -123,6 +123,7 @@ void CPlayScene::_ParseSection_OBJECTS(string line)
 	case OBJECT_TYPE_BRICK: obj = new CBrick(x,y); break;
 	case OBJECT_TYPE_COIN: obj = new CCoin(x, y); break;
 	case OBJECT_TYPE_QUESTIONBRICK: obj = new CQuestionBrick(x, y); break;
+	case OBJECT_TYPE_MUSHROOM: obj = new CMushroom(x, y); break;
 
 	case OBJECT_TYPE_PLATFORM:
 	{
