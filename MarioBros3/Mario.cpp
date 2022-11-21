26a780ba1454f00d1045ffe02e5a81e87a76d5ec
duongnhathuy99 <duongnhathuy2401@gmail.com>
2022-11-21 10:31:33 +0700

    add Mario Raccoon and update ID_ANY Mario

--- MarioBros3/Mario.cpp
+++ MarioBros3/Mario.cpp
@@ -184,72 +184,11 @@ void CMario::OnCollisionWithMushroom(LPCOLLISIONEVENT e)
 	if (MARIO_LEVEL_SMALL)
 		SetLevel(MARIO_LEVEL_BIG);
 }
-//
-// Get animation ID for small Mario
-//
-int CMario::GetAniIdSmall()
-{
-	int aniId = -1;
-	if (!isOnPlatform)
-	{
-		if (abs(ax) == MARIO_ACCEL_RUN_X)
-		{
-			if (nx >= 0)
-				aniId = ID_ANI_MARIO_SMALL_JUMP_RUN_RIGHT;
-			else
-				aniId = ID_ANI_MARIO_SMALL_JUMP_RUN_LEFT;
-		}
-		else
-		{
-			if (nx >= 0)
-				aniId = ID_ANI_MARIO_SMALL_JUMP_WALK_RIGHT;
-			else
-				aniId = ID_ANI_MARIO_SMALL_JUMP_WALK_LEFT;
-		}
-	}
-	else
-		if (isSitting)
-		{
-			if (nx > 0)
-				aniId = ID_ANI_MARIO_SIT_RIGHT;
-			else
-				aniId = ID_ANI_MARIO_SIT_LEFT;
-		}
-		else
-			if (vx == 0)
-			{
-				if (nx > 0) aniId = ID_ANI_MARIO_SMALL_IDLE_RIGHT;
-				else aniId = ID_ANI_MARIO_SMALL_IDLE_LEFT;
-			}
-			else if (vx > 0)
-			{
-				if (ax < 0)
-					aniId = ID_ANI_MARIO_SMALL_BRACE_RIGHT;
-				else if (ax == MARIO_ACCEL_RUN_X)
-					aniId = ID_ANI_MARIO_SMALL_RUNNING_RIGHT;
-				else if (ax == MARIO_ACCEL_WALK_X)
-					aniId = ID_ANI_MARIO_SMALL_WALKING_RIGHT;
-			}
-			else // vx < 0
-			{
-				if (ax > 0)
-					aniId = ID_ANI_MARIO_SMALL_BRACE_LEFT;
-				else if (ax == -MARIO_ACCEL_RUN_X)
-					aniId = ID_ANI_MARIO_SMALL_RUNNING_LEFT;
-				else if (ax == -MARIO_ACCEL_WALK_X)
-					aniId = ID_ANI_MARIO_SMALL_WALKING_LEFT;
-			}
-
-	if (aniId == -1) aniId = ID_ANI_MARIO_SMALL_IDLE_RIGHT;
-
-	return aniId;
-}
-
 
 //
 // Get animdation ID for big Mario
 //
-int CMario::GetAniIdBig()
+int CMario::GetAniId()
 {
 	int aniId = -1;
 	if (!isOnPlatform)
@@ -344,15 +283,22 @@ void CMario::Render()
 
 	if (state == MARIO_STATE_DIE)
 		aniId = ID_ANI_MARIO_DIE;
-	else if (level == MARIO_LEVEL_BIG)
-		aniId = GetAniIdBig();
 	else if (level == MARIO_LEVEL_SMALL)
-		//aniId = GetAniIdSmall();
-		aniId = GetAniIdBig()+1000;
+		aniId = GetAniId() + level * DISTANCE_ID_ANI_MARIO;
+	else if (level == MARIO_LEVEL_BIG)
+		aniId = GetAniId() + level * DISTANCE_ID_ANI_MARIO;
 	else if (level == MARIO_LEVEL_FIRE)
-		//aniId = GetAniIdSmall();
-		aniId = GetAniIdBig() + 2000;
-	animations->Get(aniId)->Render(x, y);
+		aniId = GetAniId() + level  * DISTANCE_ID_ANI_MARIO;
+	else if (level == MARIO_LEVEL_RACCOON)
+		aniId = GetAniId() + level  * DISTANCE_ID_ANI_MARIO;
+
+	if(level == MARIO_LEVEL_RACCOON)		
+		if(nx>0)
+			animations->Get(aniId)->Render(x - MARIO_RACCOON_WIDTH_ADJUST, y);
+		else
+			animations->Get(aniId)->Render(x + MARIO_RACCOON_WIDTH_ADJUST, y);
+	else
+		animations->Get(aniId)->Render(x, y);
 
 	RenderBoundingBox();
 	
